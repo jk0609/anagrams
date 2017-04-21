264ae735f4418d3ce3bb4cca0449ffc6c10e2f4b
JonathonKang <jonkang@hotmail.com>
2017-04-21 10:24:28 -0700

    rework the logic layout for answer output, unit tests need to be reworked

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,37 +1,27 @@
 class String
-#   define_method(:initialize) do |word1, word2|
-#     @word1 = word1.downcase.gsub(/[^a-z0-9]/,'')
-#     @word2 = word2.downcase.gsub(/[^a-z0-9]/,'')
-#   end
 
-  define_method(:anagram?) do |compare|
+  define_method(:anagram) do |compare|
+    (self+compare).split.each() do |word|
+      if !word[/[aeiou]/]
+        return 'One of these words needs vowels.'
+      end
+    end
     word1 = self.downcase.gsub(/[^a-z0-9]/,'')
     word2 = compare.downcase.gsub(/[^a-z0-9]/,'')
+    answer = {'anagram': "", 'palindrome': "", 'antigram': ""}
 
-    if word1[/[aeiou]/]||word2[/aeiou/]
-      if (word1.chars.sort<=>word2.chars.sort) == 0
-        true
-      else
-        false
-      end #sub branch
+    if (word1.chars.sort<=>word2.chars.sort) == 0
+      answer['anagram'] = "It's an anagram"
+      if word1.reverse == word1
+        answer['palindrome'] == " and the first word is a palindrome"
+      elsif word2.reverse == word2
+        answer['palindrome'] == " and the second word is a palindrome"
+      end#sub branch 1
     else
-      'One of these words needs vowels.'
-    end #main branch
+      answer['anagram'] = "It's not an anagram"
+      #antigram logic
+    end
+    answer
   end
 
-  define_method(:palindrome?) do
-    word1 = self.downcase.gsub(/[^a-z0-9]/,'')
-    if word1.reverse == word1
-      true
-    else
-      false
-  end
-
-  define_method(:palindrome?) do
-    word1 = self.downcase.gsub(/[^a-z0-9]/,'')
-    if word1.reverse == word1
-      true
-    else
-      false
-  end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,32 +1,26 @@
 require('rspec')
 require('./lib/anagram')
 
-describe('String#anagram?') do
-  it('returns non-anagram inputs as incorrect') do
-    expect('test'.anagram?('apple')).to(eq(false))
+describe('Word#anagram') do
+  it('returns non-anagram inputs as anagram: false') do
+    expect('test'.anagram('apple')).to(eq(false))
   end
   it('returns 2 one-word anagram inputs as true') do
-    expect('test'.anagram?('estt')).to(eq(true))
+    expect('test'.anagram('estt')).to(eq(true))
   end
   it('returns 2 one-word anagram inputs as true regardless of case') do
-    expect('TeSt'.anagram?('TsEt')).to(eq(true))
+    expect('TeSt'.anagram('TsEt')).to(eq(true))
   end
   it('returns multi-word anagram inputs as true') do
-    expect('hello again'.anagram?('ehall gioan')).to(eq(true))
+    expect('hello again'.anagram('ehall gioan')).to(eq(true))
   end
   it('rejects both inputs if either of them have no vowels') do
-    expect('srts'.anagram?('srts')).to(eq('One of these words needs vowels.'))
-  end
-end
-
-describe('String#palindrome?') do
-  it('returns false if a word is not a palindrome') do
-    expect('test'.palindrome?).to(eq(false))
+    expect('srts'.anagram('srts')).to(eq('One of these words needs vowels.'))
   end
   it('returns true if a word is a palindrome, case insensitive') do
-    expect('PotOP'.palindrome?).to(eq(true))
+    expect('PotOP'.anagram).to(eq(true))
   end
   it('returns true for multi-word palindromes, case/punct insensitive') do
-    expect('Do geese see God?'.palindrome?).to(eq(true))
+    expect('Do geese see God?'.anagram).to(eq(true))
   end
 end
