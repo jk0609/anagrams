373a48cbe790e924273971639b3fc9643a2b428f
JonathonKang <jonkang@hotmail.com>
2017-04-21 13:08:48 -0700

    minor changes to output strings, updated html for minor visibility improvements

--- app.rb
+++ app.rb
@@ -9,19 +9,19 @@ end
 
 get('/output') do
   #retrive form inputs
-  first_word = params.fetch('first_word')
-  second_word = params.fetch('second_word')
+  @first_word = params.fetch('first_word')
+  @second_word = params.fetch('second_word')
 
   #checks array values from method logic output, uses them as index values to pick the correct phrases, concatenates them onto @output
   phrases = [
-    'These words are anagrams.',
-    'These words are not anagrams.',
-    ' The first word is also a palindrome!',
-    ' The second word is also a palindrome!',
+    'These phrases are anagrams.',
+    'These phrases are not anagrams.',
+    ' The first phrase is also a palindrome!',
+    ' The second phrase is also a palindrome!',
     ' However, they are antagrams!',
     'Both words must contain vowels. Try again.'
   ]
-  output_arr = first_word.anagram(second_word)
+  output_arr = @first_word.anagram(@second_word)
   @output = ""
   output_arr.each() do |index|
     @output += phrases[index]
--- spec/anagram_integration_spec.rb
+++ spec/anagram_integration_spec.rb
@@ -9,20 +9,20 @@ describe('./lib/anagram', {:type => :feature}) do
     fill_in('first_word', :with=>'apple')
     fill_in('second_word', :with=>'papel')
     click_button('Submit')
-    expect(page).to have_content('These words are anagrams.')
+    expect(page).to have_content('These phrases are anagrams.')
   end
   it('Visits the index page, inputs "pottop" and "ppoott" and sees confirmation of anagram and first word palindrome on /output') do
     visit('/')
     fill_in('first_word', :with=>'toppot')
     fill_in('second_word', :with=>'ppoott')
     click_button('Submit')
-    expect(page).to have_content('These words are anagrams. The first word is also a palindrome!')
+    expect(page).to have_content('These phrases are anagrams. The first phrase is also a palindrome!')
   end
   it('Visits the index page, inputs "bye" and "hi ros" and sees negative confirmation on anagram but confirmation of antagram on /output') do
     visit('/')
     fill_in('first_word', :with=>'bye')
     fill_in('second_word', :with=>'hi ros')
     click_button('Submit')
-    expect(page).to have_content('These words are not anagrams. However, they are antagrams!')
+    expect(page).to have_content('These phrases are not anagrams. However, they are antagrams!')
   end
 end
